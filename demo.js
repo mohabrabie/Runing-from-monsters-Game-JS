45c2588d392e1d3e5d2b96ffa6c7a4079fdc31cd
Mohab_Rabie <mohabhamed2020@gmail.com>
2021-02-11 00:54:42 +0200

    add hitting monster and hitting character

--- demo.js
+++ demo.js
@@ -20,10 +20,20 @@ var character = {
   y:0,
   speed:10,
   health:100,
+  currentImg:"",
 }
-
+var monster = {
+  id:0,
+  x:300,
+  y:0,
+  speed:5,
+  dmg:6,
+  velX:0,
+  velY:0,
+  perSec:80,
+  health:300,
+}
 var foodListOnMap = [];
-  
 document.addEventListener('keydown', function(e) {
       if(e.code === "ArrowRight" && character.x < 1400){
         changeImge("right");
@@ -37,6 +47,48 @@ document.addEventListener('keydown', function(e) {
       }else if(e.code === "ArrowDown" && character.y < 550){
         changeImge("down");
         character.y += character.speed;
+      }else if(e.code === "Space" ){
+        if(character.currentImg === "right" || character.currentImg === "standRight")
+        {
+          var diffX = Math.floor(monster.x - character.x);
+          var diffY = Math.floor(monster.y - character.y);
+          charImg.src = "gif/hitRight.gif";
+          if(diffX < 180 && diffX > 0)
+          {
+            if(diffY < 40 && diffY > -10)
+            {
+              setTimeout(() => { charImg.src = "gif/standRight.gif"; monster.x+=300;}, 800);
+              console.log("hit the Monster");
+              monster.health-=10;
+            }
+          }
+          setTimeout(() => { charImg.src = "gif/standRight.gif"}, 800);
+          console.log(character);
+          console.log(monster);
+          console.log("dif x "+ diffX);
+          console.log("dif y "+ diffY);
+          
+        }else if(character.currentImg === "left" || character.currentImg === "standLeft"){
+          charImg.src = "gif/hitLeft.gif";
+          var diffX = Math.floor(character.x - monster.x);
+          var diffY = Math.floor(character.y - monster.y);
+          if(diffX < 180 && diffX > 0)
+          {
+            if(diffY < 40 && diffY > -10)
+            {
+              setTimeout(() => { charImg.src = "gif/standLeft.gif";monster.x-=300; }, 800);    
+              console.log("hit the Monster");
+              monster.health-=10;
+            }
+          }
+          setTimeout(() => { charImg.src = "gif/standLeft.gif";}, 800);    
+          console.log(character);
+          console.log(monster);
+          console.log("dif x "+ Math.floor(character.x - monster.x));
+          console.log("dif y "+ Math.floor(character.y - monster.y));
+          
+        }
+        
       }
       charImg.style.left = character.x + "px";
       charImg.style.top = character.y +"px";
@@ -55,15 +107,19 @@ var count;
       if(dir === "right" && count !== 1)
       {
         charImg.src = "gif/right.gif";
+        character.currentImg = "right";
         count = 1;
       }else if(dir === "left" && count !==2){
         charImg.src = "gif/left.gif";
+        character.currentImg = "left";
         count = 2;
       }else if(dir === "top" && count !=3){
         charImg.src = "gif/top.gif";
+        character.currentImg = "top";
         count = 3;
     }else if(dir === "down" && count !==4){
         charImg.src = "gif/down.gif";
+        character.currentImg = "down";
         count=4;
     }
     if(dir === "stop" && count !== 5){
@@ -73,8 +129,10 @@ var count;
         if(count === 1 || count == 4)
         {
           charImg.src = "gif/standRight.gif";
+          character.currentImg = "standRight";
         }else if(count === 2 || count == 3){
           charImg.src = "gif/standLeft.gif";
+          character.currentImg = "standLeft";
         }
         count = 5;
     }
@@ -140,26 +198,14 @@ function nextToFood(){
         }
     }
 }
-var monster = {
-  id:0,
-  x:300,
-  y:0,
-  speed:8,
-  dmg:6,
-  velX:0,
-  velY:0,
-  perSec:50,
-  img:0,
-}
 var monsterCount = 1;
-var MonsterList = [];
 function makeMonster(){
   monster.x = getRandomInt(character.x-300);
   monster.y = getRandomInt(character.y-300);
   var monsterImg = document.createElement("img");
   monsterImg.src = `monster/right.gif`;
-  monsterImg.style.width = "40px";
-  monsterImg.style.height = "40px";
+  monsterImg.style.width = "60px";
+  monsterImg.style.height = "60px";
   monsterImg.style.position = "absolute";
   monsterImg.style.left = monster.x + "px";
   monsterImg.style.top = monster.y + "px";
@@ -168,21 +214,17 @@ function makeMonster(){
   monster.img = monsterImg;
   targetX = charImg.offsetLeft;
   targetY = charImg.offsetTop;
-  monsterCount++;
-  MonsterList.push(monster);
   body.appendChild(monsterImg);
   draw(monster);
 }
-//setInterval(makeMonster,4000);
 makeMonster();
-// blue.style.left = monster.x;
-// blue.style.top = monster.y;
-var token;
+ 
+var MonsterToken=0;
+var token=0;
 function draw(monster){   
     var tx = targetX - monster.x,
         ty = targetY - monster.y,
-        dist = Math.sqrt(tx*tx+ty*ty);      
-      console.log("draw monster move");
+        dist = Math.sqrt(tx*tx+ty*ty);           
       
       monster.velX = (tx/dist)*monster.speed;
       monster.velY = (ty/dist)*monster.speed;
@@ -201,15 +243,32 @@ function draw(monster){
       img.src = `monster/left.gif`;
       token =2;
     }
-    
     img.style.left = monster.x + 'px';
     img.style.top = monster.y + 'px';
-    
+    nextToMonster()
     setTimeout(function(){draw(monster)},monster.perSec);   
 }
-function drawAllMonsters(){
-  for(let i=0;i<MonsterList.length;i++)
+function nextToMonster(){
+  var diffX = character.x - monster.x;
+  var diffY = character.y - monster.y;
+  if(diffX >= 0 && diffX <=20 || diffX >= -20 && diffX <= 0)
   {
-    draw(MonsterList[i]);
+      if((diffY >= 0 && diffY <=20 || diffY >= -20 && diffY <= 0) && MonsterToken !=1)
+      {
+        MonsterToken=1;
+        console.log("take Dmg");
+        character.x+=20;
+        character.health-=monster.dmg;
+        if(character.x >= 700)
+        {
+          character.x-=40;
+        }
+        //setTimeout(() => { console.log("DMG!"); }, 2000);
+      }else{
+        MonsterToken = 0;
+      }
   }
+}
+function takeDmg(dmg){
+  //console.log("DMG by "+monster.dmg);
 }
