cd140435a2e31fe0da109168d582f3ef5ba37e9d
sondos-medhat <56825620+sondos-medhat@users.noreply.github.com>
2021-02-12 08:37:19 +0200

    backgound levels

--- JS/levelsValues.js
+++ JS/levelsValues.js
@@ -21,13 +21,10 @@ function updateTimer() {
   {
     timesec=0;
     ++timemin;
-
-    
- 
-    // document.getElementById("timmin").innerHTML=timemin;
     // gameOver();
   }
-  console.log(foodtimeappear);
+
+//   console.log(foodtimeappear);
      setfoodtime();
 }
 function start() 
@@ -40,10 +37,19 @@ function setfoodtime(){
     if(timemin == 0){
             foodtimeappear=10000;  
             foodlvl=5;
+            document.getElementById("mainbody").classList.add('bkglvl1');
+
         }
         else if(timemin == 1 ){
             foodtimeappear=8000;
             foodlvl=4;
+            document.getElementById("mainbody").classList.remove('bkglvl1');
+            document.getElementById("mainbody").classList.add('bkglvl2');
+            // if ( document.getElementById("mainbody").classList.contains('bkglvl1') )
+            //     {
+            //         document.getElementById("mainbody").classList.toggle('bkglvl2');
+
+            //     }
         }
         else if(timemin == 2){
             foodtimeappear=6000;
@@ -56,5 +62,5 @@ function setfoodtime(){
         else{
             foodtimeappear=4000;
             foodlvl=1;
-            }
+        }
 }
